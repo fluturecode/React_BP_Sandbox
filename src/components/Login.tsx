cfe7c61fa709b6925004bc2e559c30b0fa248106
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-15 21:53:40 -0500

    added eslint puling react hooks

---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Login = () => {
+    const LoginContainer = styled.div``;
+    return (
+        <div>
+            <LoginContainer></LoginContainer>
+        </div>
+    );
+};
+
+export default Login;
