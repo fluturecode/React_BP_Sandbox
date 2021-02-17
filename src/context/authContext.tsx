40e489951607883f37eaf14e6b2f1bc37b548569
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-17 14:58:08 -0500

    updated project structure

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useReducer } from 'react';
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ reducer, initialState, children }) => (
     <AuthContext.Provider value={useReducer(reducer, initialState)}>{children}</AuthContext.Provider>
