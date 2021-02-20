d0f432dcd50c15d0d06238d596b5dfe0f37418f6
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-20 12:04:41 -0500

    edited indexContext.tsx

--- src/context/indexContext.tsx
+++ src/context/indexContext.tsx
@@ -8,25 +8,20 @@ interface Props {
     context: IContext;
 }
 
-const createDataContext: (
-    reducer: any,
-    actions: any,
-    initalState: any,
-) = (reducer, actions, initalState) => {
+const createDataContext = (reducer: any, actions: any, initalState: {}) => {
+    const Context = React.createContext<Props | null>(null);
 
-const Context = React.createContext<Props | null>(null);
+    const Provider: React.FC = ({ children }) => {
+        const [state, dispatch] = React.useReducer(reducer, initalState);
 
-const Provider: React.FC = ({ children }) => {
-    const [state, dispatch] = React.useReducer(reducer, initalState);
+        const boundActions = {};
+        for (let key in actions) {
+            boundActions[key] = actions[key](dispatch);
+        }
+        return <Context.Provider value={{ state, ...boundActions }}>{children}</Context.Provider>;
+    };
 
-    const boundActions = {};
-    for (let key in actions) {
-        boundActions[key] = actions[key](dispatch);
-    }
-    return <Context.Provider value={{ state, ...boundActions }}>{children}</Context.Provider>;
+    return { Context, Provider };
 };
-
-return { Context, Provider };
-}
 
 export default createDataContext;
