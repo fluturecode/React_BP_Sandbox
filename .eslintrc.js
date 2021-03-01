390cf18c7790a48030a6d3ae758a6a6c29fc3221
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-01 17:33:15 -0500

    updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,43 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
-  env: {
-    browser: true,
-    jest: true,
-    amd: true,
-  },
-  noempty: false,
-  novarrequires: false,
-};
+{
+  define(function (module) {
+    module.exports = {
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      rules: {
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+      plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
+      env: {
+        browser: true,
+        jest: true,
+        amd: true,
+      },
+      overrides: [
+        {
+          files: ['**/*. Ts', '**/*. Tsx'],
+          rules: {
+            'no-undef': 'off',
+          },
+        },
+      ],
+    };
+  });
+}
