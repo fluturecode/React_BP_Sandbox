2615a6d9d8fbb307bae671a1c84233292d38a030
Elizabeth Eidelson <fluturecode@gmail.com>
2021-03-03 23:45:41 -0500

    updated eslintrc.js

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-<<<<<<< Updated upstream
-=======
-import Login from '../components/login/Login';
->>>>>>> Stashed changes
-
 export const Home: React.FC = () => {
-    return (
-        <>
-            {/* <NavBar/> */}
-            {/* <Login /> */}
-            {/* <Footer/> */}
-        </>
-    );
+  return (
+    <>
+      {/* <NavBar/> */}
+      {/* <Login /> */}
+      {/* <Footer/> */}
+    </>
+  );
 };
